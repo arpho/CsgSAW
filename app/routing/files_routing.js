9a24c1cae1883af9e82be67fd93efa3cb1a7f189
arpho <damicogiuseppe77@gmail.com>
2017-02-07 19:11:29 +0100

    il processo di download è diviso in due parti, ma lo implementerò in modo più intelligente, con una direttiva che può ricevere dal server anche dei dati oltre che il solo file

--- app/routing/files_routing.js
+++ app/routing/files_routing.js
@@ -7,6 +7,15 @@ insertFile: function(file,callback)
     var registrazione = new File(file)
     registrazione.save(callback)
 },
+downloadFile : (req,res) => {
+    var folder = req.body.folder
+    var data = {result:'ok'},
+    path = require('../utilities/wrapperCache').retrieve('projectRoot') + '/app/temp/'+ folder  + '/registrazioni.zip'
+    console.log('path',path)
+    res.download(path,'registrazioni.zip', (err) => {
+        console.log('download done',err)
+    })
+},
 importFiles:function(req,res)
 {
     var fileUtilities = require('../utilities/fileUtilities'),
