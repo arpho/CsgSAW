f00524f599faceb8cd12108bfbe9a22543d22e2d
arpho <damicogiuseppe77@gmail.com>
2017-07-10 12:44:19 +0200

    refactoring
    rimosso angular spinner che dava errori

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,3 @@
 angular.module('csgSAW', ['CsgSAW.directives', 'ngMessages', 'ngMaterial', 'ng-mfb', 'ngRoute', 'appRoutes',
-    'csgSAW.controllers', 'CsgSAW.services', 'angularSpinner', 'ngCookies', 'angularMoment', 'ivh.treeview', 'ngFileUpload'
+    'csgSAW.controllers', 'CsgSAW.services', 'ngCookies', 'angularMoment', 'ivh.treeview', 'ngFileUpload'
 ]);
