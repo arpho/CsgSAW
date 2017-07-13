881bd8f0454bc0d28b2f31da91d1e7dfdd31286a
arpho <damicogiuseppe77@gmail.com>
2017-07-13 23:33:13 +0200

    middleware
    il test del modulo middleware per la gestione del token funziona, occorre rifattorizzare il codice server e client per usare il middleware.

--- app/middleware/test/tokenHandler.js
+++ app/middleware/test/tokenHandler.js
@@ -3,13 +3,18 @@ const Token = require('../../utilities/tokenGenerator');
 const Cache = require('../../utilities/wrapperCache');
 const middleware = require('../tokenHandler');
 var token = Token.generateToken();
-Cache.setCache('arpho@iol.it',token)
+Cache.setCache(token,'arpho@iol.it')
+Cache.setCache('testing',true) // setto l'ambiente di test
 var req = {}
 var res = {}
 req.body = {email:'arpho@iol.it'};
 req.body.token = token;
 var next = (req,res) => {
-    tap.equal(Cache.retrieve('arpho@iol.it'),res.out.token,'il nuovo token è'+ 
-    'incluso nella risposta e aggiornato nella cache');
+    console.log('next')
+    tap.equal(Cache.retrieve(res.out.token),'arpho@iol.it',' token ')
 };
 middleware(req,res,next)
+res.error = (txt) => {
+    tap.equal(txt,'token scaduto');
+}
+middleware(req,res);// test token scaduto
--- app/middleware/tokenHandler.js
+++ app/middleware/tokenHandler.js
@@ -1,9 +1,23 @@
-user = require('../models/User');
-Token = require('../utilities/tokenGenerator');
+const user = require('../models/User');
+const Token = require('../utilities/tokenGenerator');
+const Cache = require('../utilities/wrapperCache');
 module.exports = (req,res,next)=>{
 
     const token = req.body.token;
     const email = req.body.email;
-    console.log(token,email);
     check = Token.renewToken(token,email)
+    if(!check.valido){
+        res.error('token scaduto')
+    }
+    else{
+            res.out = {};
+            res.out.token = check.token;
+        if(!Cache.retrieve('testing')){ // non siamo in fase di test
+            next()
+        }
+        else{
+            //testing
+            next(req,res);
+        }
+    }
 }
